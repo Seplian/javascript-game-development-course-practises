dc7289ae420b15a976acbb4f3385a7eccb8cc411
lianwangstr@yandex.com <lianwangstr@yandex.com>
2022-09-26 11:35:57 +0800

    accomplish 02 project

--- 02Parallax backgrounds with JavaScript/script.js
+++ 02Parallax backgrounds with JavaScript/script.js
@@ -16,12 +16,73 @@ backgroundImage4.src = 'images/layer-4.png';
 const backgroundImage5 = new Image();
 backgroundImage5.src = 'images/layer-5.png';
 
-let x = 0;
-let x2 = 2400;
-let gameSpeed = 13;
+// let x = 0;
+// let x2 = 2400;
+let gameSpeed = 15;
+
+const gameSpeedSetter = document.querySelector('#setGameSpeed');
+gameSpeedSetter.value = gameSpeed;
+const showGameSpeed = document.querySelector('.showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+gameSpeedSetter.addEventListener('change', (e) => {
+	gameSpeed = e.target.value;
+	showGameSpeed.innerHTML = e.target.value;
+});
+
+class Layer {
+
+	constructor(image, speedModifier) {
+		this.image = image;
+		this.speedModifier = speedModifier;
+		this.x = 0;
+		this.y = 0;
+		this.width = 2400;
+		this.height = 700;
+	}
+
+	draw () {
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+	}
+
+	update() {
+		if (this.x < -this.width) {
+			this.x = 0;
+		}
+
+		// When x < -2400, then x + width < 0. The left edge of the image is already to the left of the origin. If we directly jump to x = 0, there will be a jitter to the right, so reduce the value of x regardless of whether the boundary condition is satisfied
+
+		// Remove the fractional part to make the jumps a little smoother when the boundary conditions are met
+		this.x = Math.floor(this.x -gameSpeed * this.speedModifier);
+
+		// this.x = gameFrame * gameSpeed * this.speedModifier % this.width;
+	}
+}
+
+const layer1 = new Layer(backgroundImage1, 0.2);
+const layer2 = new Layer(backgroundImage2, 0.4);
+const layer3 = new Layer(backgroundImage3, 0.6);
+const layer4 = new Layer(backgroundImage4, 0.8);
+const layer5 = new Layer(backgroundImage5, 1.0);
+const layerArray = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+	layerArray.forEach((layer) => {
+		layer.update();
+		layer.draw();
+	})
+
+	requestAnimationFrame(animate);
+}
+
+gameSpeedSetter.addEventListener('input', () => {
+	gameSpeed = e.currentTarget.value;
+});
+
+window.addEventListener('load', animate);
+
 
 // ------------------------- two variables version ------------------------
 // ------------------------- x-- in any condition -------------------------
@@ -59,21 +120,22 @@ function animate() {
 	// 	x2 -= gameSpeed;
 	// }
 
-// -------------------------- main code------------------------------------
+// -------------------------- single variable version----------------------
+// ------------------------- x-- in the else expression -------------------
 
-	ctx.drawImage(backgroundImage4, x, 0);
-	ctx.drawImage(backgroundImage4, x+2400, 0);
+	// ctx.drawImage(backgroundImage4, x, 0);
+	// ctx.drawImage(backgroundImage4, x+2400, 0);
 
-	if (x < -2400) {
-		x = 0;
-	} else {
-		x -= gameSpeed;
-	}
+	// if (x < -2400) {
+	// 	x = 0;
+	// } else {
+	// 	x -= gameSpeed;
+	// }
 
-	requestAnimationFrame(animate);
-}
 
-animate();
+
+
+
 
 // ----------------------------Some test function--------------------------
 // ------------------------- x-- in any condition -------------------------
